c857554918f0a16f3debe124d885312ffa87301e
Ola Al Zreikat <oalzre180@west-mec.org>
2025-10-08 14:28:22 -0700

    added frontend

--- config/heroInputs.config.js
+++ config/heroInputs.config.js
@@ -1,45 +1,44 @@
 // config/heroInputs.config.js
 module.exports = {
-    superName: {
-        id: 'superName',
-        name: 'superName',
-        label: 'Super Name',
-        type: 'text',
-        required: true,
-        placeholder: 'e.g., Spider-Man'
-    },
-    realName: {
-        id: 'realName',
-        name: 'realName',
-        label: 'Real Name',
-        type: 'text',
-        required: true,
-        placeholder: 'e.g., Peter Parker'
-    },
-    superpower: {
-        id: 'superpower',
-        name: 'powerLevel',
-        label: 'Power Level (1-10)',
-        type: 'number',
-        required: true,
-        min: 1,
-        max: 10
-    },
-    powerLevel: {
-        id: 'powerLevel',
-        name: 'powerLevel',
-        label: 'Power Levek (1-10)',
-        type: 'number',
-        required: true,
-        min: 1,
-        max: 10
-    },
+  superName: {
+    id: "superName",
+    name: "superName",
+    label: "Super Name",
+    type: "text",
+    required: true,
+    placeholder: "e.g., Spider-Man",
+  },
+  realName: {
+    id: "realName",
+    name: "realName",
+    label: "Real Name",
+    type: "text",
+    required: true,
+    placeholder: "e.g., Peter Parker",
+  },
+  superpower: {
+    id: "superpower",
+    name: "superpower",
+    label: "superpower",
+    type: "text",
+    required: true,
+    placeholder: "e.g., web-slinging"
+  },
+  powerLevel: {
+    id: "powerLevel",
+    name: "powerLevel",
+    label: "Power Level (1-10)",
+    type: "number",
+    required: true,
+    min: 1,
+    max: 10,
+  },
 
-    secretIdentity: {
-        id: 'secretIdentity',
-        name: 'secretIdentity',
-        label: 'Has secret identity?',
-        type: 'checkbox',
-        required: false
-    }
+  secretIdentity: {
+    id: "secretIdentity",
+    name: "secretIdentity",
+    label: "Has secret identity?",
+    type: "checkbox",
+    required: false,
+  },
 };

f89f2022ac0f0ef32834a17d1f8c4921471973d2
Ola Al Zreikat <oalzre180@west-mec.org>
2025-10-08 12:31:19 -0700

    Added frontend views files

---
+++ config/heroInputs.config.js
@@ -0,0 +1,34 @@
+module.exports = {
+    superName: {
+        id: 'superName',
+        name: 'superName',
+        label: 'Super Name',
+        type: 'text',
+        required: true,
+        placeholder: 'e.g., Spider-Man'
+    },
+    realName: {
+        id: 'realName',
+        name: 'realName',
+        label: 'Real Name',
+        type: 'text',
+        required: true,
+        placeholder: 'e.g., Peter Parker'
+    },
+    superpower: {
+        id: 'superpower',
+        name: 'powerLevel',
+        label: 'Power Level (1-10)',
+        type: 'number',
+        required: true,
+        min: 1,
+        max: 10
+    },
+    secretIdentity: {
+        id: 'secretIdentity',
+        name: 'secretIdentity',
+        label: 'Has secret identity?',
+        type: 'checkbox',
+        required: false
+    }
+};
